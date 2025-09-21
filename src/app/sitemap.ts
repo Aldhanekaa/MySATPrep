21c28bf1e569d126764ba39f57bd4f1914356cf3
Aldhanekai <aldhanekadev@gmail.com>
2025-09-21 16:21:28 +0700

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const mainDomain = "https://mysatprep.fun";
+  const mainDomain = "https://www.mysatprep.fun";
 
   return [
     {
