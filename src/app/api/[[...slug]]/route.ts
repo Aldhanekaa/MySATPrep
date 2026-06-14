30eb97db98d8e1230c763a7829e34f1882289e75
Aldhanekaa <aldhanekadev@gmail.com>
2026-06-14 21:22:19 +0700

    api migration

---
+++ src/app/api/[[...slug]]/route.ts
@@ -0,0 +1,5 @@
+export function GET() {
+  return new Response("Please Migrate to practicesat.vercel.app", {
+    status: 200,
+  });
+}
--- src/app/api/student-qb/asmEventId.ts
+++
@@ -1,5 +0,0 @@
-export const AsmEventId_to_Program = {
-  99: "SAT",
-  100: "P10",
-  102: "P89",
-};
